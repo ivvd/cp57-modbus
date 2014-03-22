962a4d85e250639ba63724a30e26b1e237f086e6
divp <ivanovdiv@gmail.com>
2014-03-22 12:51:07 +0200

    Added output function test and changed M-functinon.

--- Examples/cnc_ex1.py
+++ Examples/cnc_ex1.py
@@ -4,9 +4,18 @@ import modbus_cnc
 mb = modbus_cnc.ModbusCNC('COM30')
 
 # Setup M-function 38 as output 2 on function
-mb.writeRegister(100, 0x260005)
+#mb.writeRegister(100, 0x260005)
+
+# Setup M-function 38 as pause function
+mb.writeRegister(100, 0x260001)
 
 # Setup inputs
-mb.writeRegister(136, 11) # input 4 - homing
-mb.writeRegister(137, 12) # input 5 - start program from reg90
+mb.writeRegister(136, 11) # input 5 - homing
+mb.writeRegister(137, 12) # input 6 - start program (number in reg90)
 mb.writeRegister(90, 2) # reg90 = program 2
+mb.writeRegister(138, 23) # input 7 - stop
+mb.writeRegister(139, 24) # input 8 - block
+
+# Setup outputs
+mb.writeRegister(140, 25) # output 6 - error
+mb.writeRegister(141, 26) # output 7 - motion
